5fa7fa8d7a4f0b60e23bf42a5ddb1932986db085
AnonBD <99282155+anonbd1@users.noreply.github.com>
2022-04-01 15:48:41 -0400

    Update index.php

--- index.php
+++ index.php
@@ -3,40 +3,6 @@ You try to copy!? Copy With Credit🧣
 
 Design By @ANONBD
 -->
-<?php session_start(); /* Starts the session */
-
-if(!isset($_SESSION['UserData']['Username'])){
-	header("location:login.php");
-	exit;
-}
-
-if (!empty($_SERVER['HTTP_CLIENT_IP']))
-    {
-      $ipaddress = $_SERVER['HTTP_CLIENT_IP']."\r\n";
-    }
-elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))
-    {
-      $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR']."\r\n";
-    }
-else
-    {
-      $ipaddress = $_SERVER['REMOTE_ADDR']."\r\n";
-    }
-$useragent = " User-Agent: ";
-$browser = $_SERVER['HTTP_USER_AGENT'];
-
-
-$file = 'ip.txt';
-$victim = "IP: ";
-$fp = fopen($file, 'a');
-
-fwrite($fp, $victim);
-fwrite($fp, $ipaddress);
-fwrite($fp, $useragent);
-fwrite($fp, $browser);
-
-
-fclose($fp);
 <!doctype html>
 <html lang="en">
   <head>
